4c2918058af2223fcc27ef2717ad55afc9d31e5b
Jhon Gutierrez <sisoemp.developer02@outlook.com>
2022-03-29 09:02:19 -0500

    JGB Ajustes de reporte

--- Application/Features/ReportesFeatures/Queries/GetReportesByFechaQuery.cs
+++ Application/Features/ReportesFeatures/Queries/GetReportesByFechaQuery.cs
@@ -9,27 +9,60 @@ using System.Threading.Tasks;
 
 namespace Application.Features.ReportesFeatures.Queries
 {
-    public class GetReportesByFechaQuery : IRequest<IEnumerable<JObject>>
+    public class GetReportesByFechaQuery : IRequest<IEnumerable<object>>
     {
         public DateTime Fecha { get; set; }
 
-        public class GetReportesByFechaQueryHandler : IRequestHandler<GetReportesByFechaQuery, IEnumerable<JObject>>
+        public class GetReportesByFechaQueryHandler : IRequestHandler<GetReportesByFechaQuery, IEnumerable<object>>
         {
             private readonly IMovimientoRepository _movimientoRepository;
-            public GetReportesByFechaQueryHandler(IMovimientoRepository movimientoRepository)
+            private readonly ICuentaRepository _cuentaRepository;
+            private readonly IClienteRepository _clienteRepository;
+            public GetReportesByFechaQueryHandler(
+                IMovimientoRepository movimientoRepository,
+                ICuentaRepository cuentaRepository,
+                IClienteRepository clienteRepository
+                )
             {
                 _movimientoRepository = movimientoRepository;
+                _cuentaRepository = cuentaRepository;
+                _clienteRepository = clienteRepository;
             }
 
-            public async Task<IEnumerable<JObject>> Handle(GetReportesByFechaQuery query, CancellationToken cancellationToken)
+            public async Task<IEnumerable<object>> Handle(GetReportesByFechaQuery query, CancellationToken cancellationToken)
             {
+                List<object> reporte = null;
                 var movimientos = _movimientoRepository.GetAll();
 
                 if (movimientos == null)
                 {
                     return null;
                 }
-                return null;
+
+                reporte = new List<object>();
+                DateTime now = DateTime.Now;
+
+                foreach(var movimiento in movimientos)
+                {
+                    if(movimiento.Fecha.ToString("dd/MM/yyyy") == now.ToString("dd/MM/yyyy"))
+                    {
+                        var cuenta = await _cuentaRepository.GetById(movimiento.CuentaId);
+                        var cliente = await _clienteRepository.GetById(cuenta.ClienteId);
+                        reporte.Add(new 
+                        {
+                            Fecha = movimiento.Fecha.ToString("dd/MM/yyyy"),
+                            Cliente= cliente.Nombre ,
+                            NumeroCuenta = cuenta.NumeroCuenta ,
+                            Tipo = cuenta.TipoCuenta == Domain.Entity.Cuenta.TipoCuentas.AHORRO ? "Ahorro" : "Corriente",
+                            SaldoInicial = cuenta.SaldoInicial,
+                            Estado = cuenta.Estado,
+                            Movimiento = movimiento.Valor,
+                            SaldoDisponible = movimiento.Saldo
+                        });
+                    }
+                }
+
+                return reporte;
             }
         }
     }
